e636b091c0bf0779af36c695e2b87edd0f28bfc0
arnunez2002 <70178954+arnunez2002@users.noreply.github.com>
2021-11-05 11:49:29 -0500

    Update controller.java

--- src/co/edu/unbosque/controller/controller.java
+++ src/co/edu/unbosque/controller/controller.java
@@ -1,4 +1,37 @@
 package co.edu.unbosque.controller;
 
-public class controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import co.edu.unbosque.view.PrincipalView;
+
+public class controller implements ActionListener {
+
+	private PrincipalView vista;
+	
+	public  controller() {
+		// TODO Auto-generated constructor stub
+		vista = new PrincipalView(this);
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource()==vista.getFloyd()) {
+			System.out.println("Algoritmo de Floyd");
+        }
+        if (e.getSource()==vista.getViajero()) {
+    		System.out.println("El problema del viajero");
+        }
+        if (e.getSource()==vista.getMatrices()) {
+        	System.out.println("Multiplicación de matrices");
+        }
+        if (e.getSource()==vista.getAsignaciónTareas()) {
+          	System.out.println("El problema de asignación de tareas");
+        }
+        if (e.getSource()==vista.getCoeficientesBinomiales()) {
+          	System.out.println("Coeficientes Binomiales");
+        }
+	}
 }
